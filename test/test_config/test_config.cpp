bea1b7ac75e279f7505b6dd48247c7fda386710d
chl33 <64091421+chl33@users.noreply.github.com>
2024-02-09 13:06:20 -0500

    moved test_json_parse from test_config to test_variables (#5)

--- test/test_config/test_config.cpp
+++ test/test_config/test_config.cpp
@@ -4,6 +4,7 @@
 #include <ArduinoFake.h>
 
 #include "og3/config_interface.h"
+#include "og3/constants.h"
 #include "og3/flash_support.h"
 #include "og3/logger.h"
 #include "og3/module_system.h"
@@ -53,9 +54,46 @@ void test_config() {
   TEST_ASSERT_EQUAL_STRING("home", loc.value().c_str());
 }
 
+void test_json_parse() {
+  constexpr unsigned kCfgSet =
+      og3::VariableBase::Flags::kConfig | og3::VariableBase::Flags::kSettable;
+  og3::VariableGroup vg("watering");
+  og3::FloatVariable max_moisture_target("max_moisture_target", 80.0f, og3::units::kPercentage,
+                                         "Max moisture", kCfgSet, 0, &vg);
+  og3::FloatVariable min_moisture_target("min_moisture_target", 70.0f, og3::units::kPercentage,
+                                         "Min moisture", kCfgSet, 0, &vg);
+  og3::FloatVariable pump_dose_msec("pump_on_msec", 3 * og3::kMsecInSec, og3::units::kMilliseconds,
+                                    "Pump on time", kCfgSet, 0, &vg);
+  og3::BoolVariable watering_enabled("watering_enabled", false, "watering enabled", kCfgSet, &vg);
+  og3::BoolVariable reservoir_check_enabled("res_check_enabled", false, "reservior check enabled",
+                                            kCfgSet, &vg);
+  og3::FloatVariable pump_seconds_after_low("pump_after_low", 10.0f, og3::units::kSeconds,
+                                            "pump seconds after low water", kCfgSet, 0, &vg);
+  const char json[] =
+      "{\"soil_moisture_in_min\":2900,\"soil_moisture_in_max\":1470,\"soil_moisture_out_min\":0,"
+      "\"soil_moisture_out_max\":100,\"soil_moisture_delta_per_deg\":0.075000003,\"max_moisture_"
+      "target\":75,\"min_moisture_target\":65,\"pump_on_msec\":3000,\"watering_enabled\":\"true\","
+      "\"res_check_enabled\":\"true\",\"pump_after_low\":10}";
+
+  JsonDocument doc;
+  deserializeJson(doc, json);
+  for (auto* var : vg.variables()) {
+    if (!var->config()) {
+      continue;
+    }
+    const JsonVariant val = (doc)[var->name()];
+    if (val.isNull()) {
+      continue;
+    }
+    TEST_ASSERT_TRUE(var->fromJson(val));
+  }
+  TEST_ASSERT_TRUE(reservoir_check_enabled.value());
+}
+
 int runUnityTests() {
   UNITY_BEGIN();
   RUN_TEST(test_config);
+  RUN_TEST(test_json_parse);
   return UNITY_END();
 }
 
