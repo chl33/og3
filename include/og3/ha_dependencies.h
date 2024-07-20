b57936221533e5c34055d74c80035baa24d2e470
Chris Lee <lee.chris.h@gmail.com>
2024-07-19 21:15:04 -0400

    add HADependenciesArray class

--- include/og3/ha_dependencies.h
+++ include/og3/ha_dependencies.h
@@ -31,4 +31,30 @@ class HADependencies : public Dependencies {
   HADiscovery* m_ha_discovery = nullptr;
 };
 
+template <unsigned N>
+class HADependenciesArray : public HADependencies {
+ public:
+  HADependenciesArray(const std::array<const char*, N>& module_names) : m_more_deps(module_names) {}
+  bool resolve(const NameToModule& name_to_module) override;
+  size_t num_depends_on() const override {
+    return HADependencies::num_depends_on() + m_more_deps.num_depends_on();
+  }
+  const Module* depends_on(size_t idx) const override {
+    const auto d1 = HADependencies::num_depends_on();
+    return idx < d1 ? HADependencies::depends_on(idx) : m_more_deps.depends_on(idx - d1);
+  }
+
+  MqttManager* mqtt_manager() { return m_mqtt_manager; }
+  const MqttManager* mqtt_manager() const { return m_mqtt_manager; }
+  HADiscovery* ha_discovery() { return m_ha_discovery; }
+  const HADiscovery* ha_discovery() const { return m_ha_discovery; }
+
+  const bool ok() { return m_mqtt_manager && m_ha_discovery; }
+
+ protected:
+  MqttManager* m_mqtt_manager = nullptr;
+  HADiscovery* m_ha_discovery = nullptr;
+  DependenciesArray<N> m_more_deps;
+};
+
 }  // namespace og3
