5cfd711cecebb4a7e74ed87d066e36af1560455c
Chris Lee <lee.chris.h@gmail.com>
2025-06-08 16:49:39 -0400

    new classes: AdcVoltage and ConfigModule

---
+++ include/og3/adc_voltage.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <og3/config_module.h>
+#include <og3/mapped_analog_sensor.h>
+
+namespace og3 {
+
+class AdcVoltage : public ConfigModule {
+  // voltage divider with 10k, 20k(mez) R.
+ public:
+  AdcVoltage(const char* name, WifiApp* app, uint8_t pin, const char* raw_desc, const char* desc,
+             float out_max, VariableGroup& vg, VariableGroup& config_vg);
+
+  float read() { return m_mapped_adc.read(); }
+  float value() const { return m_mapped_adc.value(); }
+  const FloatVariable& valueVariable() const { return m_mapped_adc.mapped_value(); }
+  int raw_counts() const { return m_mapped_adc.raw_counts(); }
+
+ private:
+  static constexpr unsigned kCfgSet = VariableBase::Flags::kConfig | VariableBase::Flags::kSettable;
+
+  MappedAnalogSensor m_mapped_adc;
+};
+
+}  // namespace og3
---
+++ include/og3/config_module.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2025 Chris Lee and contibuters.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+#pragma once
+
+#include "og3/config_interface.h"
+#include "og3/dependencies.h"
+#include "og3/module.h"
+#include "og3/variable.h"
+#include "og3/web_server.h"
+#include "og3/wifi_app.h"
+
+namespace og3 {
+
+// ConfigModule is a parent class for modules which save and load their variable values
+//  from flash via the config system and should have their own web page for setting
+//  variable values.
+class ConfigModule : public Module {
+ public:
+  ConfigModule(const char* name, WifiApp* app);
+  const char* cfg_url() const { return m_cfg_url.c_str(); }
+  void add_html_button(String* body) const;
+
+ protected:
+  const WifiApp* m_app;
+  DependenciesArray<2> m_config_module_deps;
+  VariableGroup m_cvg;
+  ConfigInterface* m_config = nullptr;
+  WebServer* m_web_server = nullptr;
+  std::string m_cfg_url;
+  String m_html;
+
+ private:
+  void handleConfigRequest(AsyncWebServerRequest* request);
+};
+
+}  // namespace og3
