a946daf4cf3717c7136c083d548288bd65943619
Chris Lee <lee.chris.h@gmail.com>
2025-10-18 16:31:10 -0400

    dependencies that build

--- util/ci.sh
+++ util/ci.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 set -e
 cd "$(dirname "$0")"/..
-pio ci --board d1_mini --lib . examples/blink/blink.cpp
-pio ci --board d1_mini --lib . examples/wifi-app/wifi-app.cpp
-pio ci --board d1_mini --lib . examples/web-app/web-app.cpp
-pio ci --board d1_mini --lib . examples/ha-app/ha-app.cpp
+BOARD=d1_mini
+#BOARD=esp32dev
+pio ci --board "${BOARD}" --lib . examples/blink/blink.cpp
+pio ci --board "${BOARD}" --lib . examples/wifi-app/wifi-app.cpp
+pio ci --board "${BOARD}" --lib . examples/web-app/web-app.cpp
+pio ci --board "${BOARD}" --lib . examples/ha-app/ha-app.cpp
