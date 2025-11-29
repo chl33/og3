8ebb7708165b5d611ea84e7197815faf157b816c
Chris Lee <lee.chris.h@gmail.com>
2025-11-29 09:55:15 -0500

    added license header

--- test/test_deps_api/test_deps_api.cpp
+++ test/test_deps_api/test_deps_api.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Chris Lee and contibuters.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
 #include <unity.h>
 
 #include "og3/logger.h"
