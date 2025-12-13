d61c02f65efd199e5d4321c1be9a91f637bac737
Chris Lee <lee.chris.h@gmail.com>
2025-12-13 12:41:57 -0500

    ci update

--- util/ci.sh
+++ util/ci.sh
@@ -7,17 +7,21 @@ cd "$(dirname "$0")"/..
 echo "Building for d1_mini..."
 BOARD=d1_mini
 FLAGS='lib_ignore=AsyncTCP_RP2040W,AsyncTCP'
-pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" examples/blink/blink.cpp
-pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" examples/wifi-app/wifi-app.cpp
-pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" examples/web-app/web-app.cpp
-pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" examples/ha-app/ha-app.cpp
+COMPAT='lib_compat_mode=strict'
+pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" --project-option="${COMPAT}" examples/blink/blink.cpp
+pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" --project-option="${COMPAT}" examples/wifi-app/wifi-app.cpp
+pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" --project-option="${COMPAT}" examples/web-app/web-app.cpp
+pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" --project-option="${COMPAT}" examples/ha-app/ha-app.cpp
 
 # Build for esp32dev (ESP32)
 # Ignores AsyncTCP_RP2040W (for Pico W)
 echo "Building for esp32dev..."
-BOARD=esp32dev
-FLAGS='lib_ignore=AsyncTCP_RP2040W'
-pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" examples/blink/blink.cpp
-pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" examples/wifi-app/wifi-app.cpp
-pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" examples/web-app/web-app.cpp
-pio ci --board "${BOARD}" --lib . --project-option="${FLAGS}" examples/ha-app/ha-app.cpp
+pio run -e esp32dev_blink
+pio run -e esp32dev_wifi_app
+pio run -e esp32dev_web_app
+pio run -e esp32dev_ha_app
+
+pio run -e lolin_c3_mini_blink
+pio run -e lolin_c3_mini_wifi_app
+pio run -e lolin_c3_mini_web_app
+pio run -e lolin_c3_mini_ha_app
