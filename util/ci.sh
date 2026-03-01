71a54f43d545ec801fcf31fe2fe1cb5efb1128db
Chris Lee <lee.chris.h@gmail.com>
2026-02-28 20:02:12 -0500

    CI: test on one ESP8266 and one ESP32 board

--- util/ci.sh
+++ util/ci.sh
@@ -2,15 +2,10 @@
 set -e
 cd "$(dirname "$0")"/..
 
-# Build for d1_mini (ESP8266)
-# Ignores AsyncTCP (for ESP32) and AsyncTCP_RP2040W (for Pico W)
-echo "Building for d1_mini..."
-BOARD=d1_mini
-FLAGS='lib_ignore=AsyncTCP_RP2040W,AsyncTCP'
-COMPAT='lib_compat_mode=strict'
-
-for board in d1_mini esp32dev lolin; do
+for board in d1_mini esp32dev; do
+    echo "-- Building for ${board}..."
     for app in blink wifi_app web_app ha_app; do
+	echo " -- Building ${board}_${app}..."
 	pio run -e "${board}_${app}"
     done
 done
