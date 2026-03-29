def1be0a8bfe1c5d5567f28d0991777be9432b4b
Chris Lee <lee.chris.h@gmail.com>
2026-03-29 13:59:41 -0400

    pid: use camelCase for variable string-names

--- src/pid.cpp
+++ src/pid.cpp
@@ -12,24 +12,24 @@ constexpr unsigned kOutputSet = 0;
 
 PID::PID(const PID::Gains& gains, VariableGroup& vg, VariableGroup& cfg_vg, VariableGroup& cmd_vg)
     : m_cfg_vg(cfg_vg),
-      m_p("p", gains.p, "", "proportional gain", kCfgSet, 2, cfg_vg),
-      m_i("i", gains.i, "", "integral gain", kCfgSet, 5, cfg_vg),
-      m_d("d", gains.d, "", "derivative gain", kCfgSet, 2, cfg_vg),
-      m_i_max("i_max", gains.i_max, "", "maximum integral val", kCfgSet, 1, cfg_vg),
-      m_i_min("i_min", gains.i_min, "", "minimum integral val", kCfgSet, 1, cfg_vg),
+      m_p("kP", gains.p, "", "proportional gain", kCfgSet, 2, cfg_vg),
+      m_i("kI", gains.i, "", "integral gain", kCfgSet, 5, cfg_vg),
+      m_d("kD", gains.d, "", "derivative gain", kCfgSet, 2, cfg_vg),
+      m_i_max("iMax", gains.i_max, "", "maximum integral val", kCfgSet, 1, cfg_vg),
+      m_i_min("iMin", gains.i_min, "", "minimum integral val", kCfgSet, 1, cfg_vg),
       m_feedforward("feedforward", gains.feedforward, "", "feedforward", kCfgSet, 1, cfg_vg),
-      m_command_min("command_min", gains.command_min, "", "minimum output", kCfgSet, 1, cfg_vg),
-      m_command_max("command_max", gains.command_max, "", "maximum output", kCfgSet, 1, cfg_vg),
+      m_command_min("commandMin", gains.command_min, "", "minimum output", kCfgSet, 1, cfg_vg),
+      m_command_max("commandMax", gains.command_max, "", "maximum output", kCfgSet, 1, cfg_vg),
       m_last_msec(millis()),
       m_target("target", 25.0f, "", "control target", kCfgSet, 1, cmd_vg),
-      m_d_target("d_target", 0.0f, "", "control target derivative", VariableBase::Flags::kConfig, 1,
+      m_d_target("dTarget", 0.0f, "", "control target derivative", VariableBase::Flags::kConfig, 1,
                  cmd_vg),
       m_error("error", 0.0f, "", "error", kOutputSet, 1, vg),
-      m_d_error("d_error", 0.0f, "", "derivative error", kOutputSet, 1, vg),
+      m_d_error("dError", 0.0f, "", "derivative error", kOutputSet, 1, vg),
       m_command("command", 0.0f, "", "control output", kOutputSet, 1, vg),
-      m_p_cmd("p_cmd", 0.0f, "", "proportional command component", kOutputSet, 1, vg),
-      m_d_cmd("d_cmd", 0.0f, "", "derivative command component", kOutputSet, 1, vg),
-      m_i_cmd("i_cmd", 0.0f, "", "integral command component", kOutputSet, 1, vg) {}
+      m_p_cmd("pCmd", 0.0f, "", "proportional command component", kOutputSet, 1, vg),
+      m_d_cmd("dCmd", 0.0f, "", "derivative command component", kOutputSet, 1, vg),
+      m_i_cmd("iCmd", 0.0f, "", "integral command component", kOutputSet, 1, vg) {}
 
 float PID::command(float measured, unsigned now_msec) { return command(measured, 0.0f, now_msec); }
 
